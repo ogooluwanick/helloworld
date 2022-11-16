ffc7ef746b15fc2fc5ec6a4686e100afc3acebf1
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-16 13:03:09 +0100

    learnt comments

--- helloscript.sh
+++ helloscript.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
-# echo "Bash script" > file.text
+# echo "Bash script" 													#display text 
 
-cat >> file.text
+# echo "Bash script" > file.text 										#display text in file
+
+# cat > file.text														#reset file & add text
+
+# cat >> file.text 														#append text
+
+#comment 																#comment 
+
+cat << textComment														#muiltiline comment
+This is test text your script ran good thanks
+textComment
